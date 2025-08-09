1fd020abebf7f09643a87348f44fdc471c81860f
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-08-09 17:12:44 +0300

    [flake] add package, cleanup

--- flake.nix
+++ flake.nix
@@ -1,52 +1,56 @@
 {
-  description = "Zsh full build environment (with ncurses, Rust, etc.)";
+  description = "rsmetrx - small Rust utilities for system metrics";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:NixOS/nixpkgs";
     flake-utils.url = "github:numtide/flake-utils";
-    rust-overlay.url = "github:oxalica/rust-overlay";
   };
 
-  outputs = { self, nixpkgs, flake-utils, rust-overlay }:
+  outputs = { self, nixpkgs, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = import nixpkgs {
-          inherit system;
-          overlays = [ rust-overlay.overlays.default ];
+        pkgs = import nixpkgs { inherit system; };
+      in rec {
+        # Build the main binary from Cargo.toml/Cargo.lock in repo root
+        packages.default = pkgs.rustPlatform.buildRustPackage {
+          pname = "rsmetrx";
+          version = "0.1.0";
+          src = ./.;
+
+          # Use the checked-in lockfile for reproducible builds
+          cargoLock = {
+            lockFile = ./Cargo.lock;
+          };
+
+          # Add native deps here if needed (e.g. pkg-config)
+          nativeBuildInputs = [ ];
+          buildInputs = [ ];
+
+          # Optional metadata
+          meta = with pkgs.lib; {
+            description = "System metrics CLI utilities";
+            homepage = "https://github.com/neg-serg/rsmetrx";
+            license = licenses.mit;
+            maintainers = [ maintainers.undefined ];
+            mainProgram = "rsmetrx";
+          };
         };
-        rust = pkgs.rust-bin.stable.latest.default;
-      in
-      {
+
+        # Enable `nix run .#rsmetrx`
+        apps.default = {
+          type = "app";
+          program = "${packages.default}/bin/rsmetrx";
+        };
+
+        # Dev shell with common Rust tooling
         devShells.default = pkgs.mkShell {
-          buildInputs = [
-            rust
-            pkgs.zsh
-            pkgs.pkg-config
-            pkgs.tree-sitter
-            pkgs.lscolors
-            # Full C build env for zsh:
-            pkgs.gcc
-            pkgs.gnumake
-            pkgs.autoconf
-            pkgs.automake
-            pkgs.perl
-            pkgs.patch
-            pkgs.ncurses
-            pkgs.ncurses.dev
-            pkgs.readline
-            pkgs.readline.dev
-            pkgs.libcap
-            pkgs.libcap.dev
-            pkgs.bison
-            pkgs.flex
-            pkgs.gettext
-            pkgs.util-linux # for 'script' tool etc.
+          nativeBuildInputs = with pkgs; [
+            cargo
+            rustc
+            rustfmt
+            clippy
           ];
-          shellHook = ''
-            echo "Zsh build environment ready!"
-          '';
         };
       }
     );
 }
-
